f0415ad6ad37f81d71d982097494be678a9a5195
CaroBorges <caboes18685@gmail.com>
2014-10-22 00:26:39 -0600

    style gallery

--- application/views/escazu_gallery_view.php
+++ application/views/escazu_gallery_view.php
@@ -1,7 +1,7 @@
 <div id="divMainContentGallery" ng-controller="galleryController" ng-init="loadGallery()">
 	<div class="flexslider">
 	  <ul class="slides">
-	    <li data-thumb="slide1-thumb.jpg">
+	    <li data-thumb="<?php echo base_url(); ?>img/f5-escazu-button.png">
 	      <img src="<?php echo base_url(); ?>img/f5-escazu-button.png"/>
 	    </li>
 	    <li data-thumb="slide2-thumb.jpg">
