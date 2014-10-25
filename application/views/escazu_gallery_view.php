04dc9a59d4ea79f1e3e17f9348f2aa4320c9abc4
CaroBorges <caboes18685@gmail.com>
2014-10-24 23:27:56 -0600

    gallery style

--- application/views/escazu_gallery_view.php
+++ application/views/escazu_gallery_view.php
@@ -1,60 +1,31 @@
 <div id="divMainContentGallery" ng-controller="galleryController" ng-init="loadGallery()">
 	<h1>Galleria</h1>
 		<div id="slider" class="flexslider">
-		  <ul class="slides">
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_1.jpg"/>
-		    </li>
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_2.jpg" />
-		    </li>
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_3.jpg" />
-		    </li>
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_1.jpg"/>
-		    </li>
+		 	<ul class="slides">
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_2.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_3.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_2.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_3.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
 		    <!-- items mirrored twice, total of 12 -->
-		  </ul>
+		  	</ul>
 		</div>
+
 		<div id="carousel" class="flexslider">
-		  <ul class="slides">
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_1.jpg" />
-		    </li>
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_2.jpg" />
-		    </li>
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_3.jpg"/>
-		    </li>
-		    <li>
-		      <img src="<?php echo base_url(); ?>img/picture_1.jpg" />
-		    </li>
+		 	<ul class="slides">
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_2.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_3.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_2.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_3.jpg"/></li>
+			    <li><img src="<?php echo base_url(); ?>img/picture_1.jpg"/></li>
 		    <!-- items mirrored twice, total of 12 -->
-		  </ul>
-	</div>
-
-
-
-
-
-
-
-	<!--<div class="flexslider">
-	  <ul class="slides">
-	    <li data-thumb="<?php echo base_url(); ?>img/picture_1.jpg">
-	      <img src="<?php echo base_url(); ?>img/picture_1.jpg"/>
-	    </li>
-	    <li data-thumb="<?php echo base_url(); ?>img/picture_2.jpg">
-	      <img src="<?php echo base_url(); ?>img/picture_2.jpg"/>
-	    </li>
-	    <li data-thumb="<?php echo base_url(); ?>img/picture_3.jpg">
-	      <img src="<?php echo base_url(); ?>img/picture_3.jpg"/>
-	    </li>
-	    <li data-thumb="<?php echo base_url(); ?>img/picture_1.jpg">
-	      <img src="<?php echo base_url(); ?>img/picture_1.jpg"/>
-	    </li>
-	  </ul>
-	</div>-->
+			</ul>
+		</div>
 </div>
+
