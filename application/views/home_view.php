ebd8eebb876f20f71eee96b5ad7a819a2697bac2
CaroBorges <caboes18685@gmail.com>
2014-10-26 20:12:30 -0600

    content style perfect pixel

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,6 +1,6 @@
     <div id="divContent">
         <h1>Canchas de futbol 5</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur, nesciunt dicta, esse rem ducimus itaque quis. Adipisci ullam nam qui illum debitis sit ad in lectus. 
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil conseq tuuntur, nesciunt dicta, esse rem ducimus itaque quis. Adipisci ullam nam qui illum debitis sit ad in lectus. 
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur, nesciunt dicta.
         </p>
         <h2>¿Donde quiere jugar?</h2>
