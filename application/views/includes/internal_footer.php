fea2ead899432dd23103dbc4809452e7934d75c1
Christian Campos <christian.campos@prodigious.cr>
2014-10-28 20:10:57 -0600

    footer modified

--- application/views/includes/internal_footer.php
+++ application/views/includes/internal_footer.php
@@ -9,19 +9,19 @@
 		    	<div id="contentSocialIcons" class="clearfix">
 		    		<span>S&iacute;guenos en:</span>
 		    		<div id="socialIcons">
-			            <a href="" class="fa fa-facebook"></a>
-			            <a href="" class="fa fa-twitter"></a>
-			            <a href="" class="fa fa-youtube"></a>  
+			            <a href="#" class="fa fa-facebook"></a>
+			            <a href="#" class="fa fa-twitter"></a>
+			            <a href="#" class="fa fa-youtube"></a>  
 			        </div>	     
 		    	</div>	       
 
 		    	<div id="infoContact" class="clearfix">
 		    		<span>Cont&aacute;ctenos:</span> 	 
 			    	<ul>
-			    		<li><span class=" fa fa-map-marker"></span><a href="<?php echo $mapUrl ?>">Localizaci&oacute;n</a></li>
-			            <li><span class=" fa fa-phone-square"></span><a href="tel:<?php echo $phone ?>"><?php echo $phone ?></a></li>
-			            <li><span class=" fa fa-envelope"></span><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></li>
-			            <li><span class=" fa fa-skype"></span><a href="skype:<?php echo $skype ?>?call"><?php echo $skype ?></a></li>
+			    		<li><span class="fa fa-map-marker"></span><a href="<?php echo $mapUrl ?>">Localizaci&oacute;n</a></li>
+			            <li><span class="fa fa-phone-square"></span><a href="tel:<?php echo $phone ?>"><?php echo $phone ?></a></li>
+			            <li><span class="fa fa-envelope"></span><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></li>
+			            <li><span class="fa fa-skype"></span><a href="skype:<?php echo $skype ?>?call"><?php echo $skype ?></a></li>
 			    	</ul>
 		            
 			    </div>
