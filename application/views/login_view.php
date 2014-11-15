34670f153cd34a3044672736cc0845d85d4fdcb9
CaroBorges <carobe18685@gmail.com>
2014-11-15 00:36:12 -0600

    style login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,13 +1,15 @@
-<div id="contentLogin" id="divContent">
-   <h1 id="loginTitle">Login</h1>
-   <?php echo validation_errors(); ?>
-   <?php echo form_open($this->uri->segment(1) . '/verifyLogin'); 
-   $label_attributes = array('class' => 'address_info');
-   echo form_label("Username: ",'',$label_attributes);
-   echo form_input("username");
-   echo form_label("Password: ");
-   echo form_password("password");
-   echo form_submit("","Login");
-   echo form_close();
-   ?>
+<div id="divContent">
+   <div id="contentLogin">
+      <h1 id="loginTitle">Login</h1>
+      <?php echo validation_errors(); ?>
+      <?php echo form_open($this->uri->segment(1) . '/verifyLogin'); 
+      echo form_label("Username: ");
+      echo form_input("username",'','class="form-control"');
+      echo form_label("Password: ");
+      echo form_password("password",'','class="form-control"');
+      echo form_submit("","Login",'class="btn btn-danger" id="btnLogin"');
+      echo form_close();
+      ?>
+   </div>
 </div>
+
