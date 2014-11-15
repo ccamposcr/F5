a4c836555980dbbcc7fa4520ad8bd0980c9ea07d
CaroBorges <carobe18685@gmail.com>
2014-11-14 20:36:23 -0600

    style login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,8 +1,9 @@
-<div id="divContent">
+<div id="contentLogin" id="divContent">
    <h1 id="loginTitle">Login</h1>
    <?php echo validation_errors(); ?>
    <?php echo form_open($this->uri->segment(1) . '/verifyLogin'); 
-   echo form_label("Username: ");
+   $label_attributes = array('class' => 'address_info');
+   echo form_label("Username: ",'',$label_attributes);
    echo form_input("username");
    echo form_label("Password: ");
    echo form_password("password");
