5388a3c3f75cc7d04569c049a8728e71bbad1172
CaroBorges <carobe18685@gmail.com>
2014-11-26 17:59:41 -0600

    title home style

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,7 +3,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil conseq tuuntur, nesciunt dicta, esse rem ducimus itaque quis. Adipisci ullam nam qui illum debitis sit ad in lectus. 
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur, nesciunt dicta.
         </p>
-        <h2>¿Donde quiere jugar?</h2>
+        <h2><span>¿</span>Donde quiere jugar<span>?</span></h2>
         <div id="divContentButtons" class="clearfix">
             <div id="divButton1">
                 <a href="<?php echo base_url(); ?>escazu/1/reservaciones"><img id="imgLogoBoton1" src="<?php echo base_url(); ?>img/f5-escazu-button1.png" alt="F5" height="98" width="198"></a>
