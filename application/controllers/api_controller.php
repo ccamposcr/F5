f6bf4d42dc62358628986f6d0c2964e2d89c141c
Christian Campos <christian.campos@prodigious.cr>
2014-12-02 13:28:00 -0600

    xxs filtering

--- application/controllers/api_controller.php
+++ application/controllers/api_controller.php
@@ -9,59 +9,59 @@ class Api_controller extends CI_Controller {
     }
 
     public function getReservationByTime(){
-        $reservation_year = ( isset($_POST['reservation_year']) ) ? $_POST['reservation_year'] : date("Y", time());
-        $reservation_month = ( isset($_POST['reservation_month']) ) ? $_POST['reservation_month'] : date("m", time());
-        $reservation_day = ( isset($_POST['reservation_day']) ) ? $_POST['reservation_day'] : date("d", time());
-        $reservation_time = ( isset($_POST['reservation_time']) ) ? $_POST['reservation_time'] : '08-09';
-        $group_id = ( isset($_POST['group_id']) ) ? $_POST['group_id'] : 1;
-        $pitch_id = ( isset($_POST['pitch_id']) ) ? $_POST['pitch_id'] : 1;
+        $reservation_year = ( isset($_POST['reservation_year']) ) ? strip_tags($_POST['reservation_year']) : date("Y", time());
+        $reservation_month = ( isset($_POST['reservation_month']) ) ? strip_tags($_POST['reservation_month']) : date("m", time());
+        $reservation_day = ( isset($_POST['reservation_day']) ) ? strip_tags($_POST['reservation_day']) : date("d", time());
+        $reservation_time = ( isset($_POST['reservation_time']) ) ? strip_tags($_POST['reservation_time']) : '08-09';
+        $group_id = ( isset($_POST['group_id']) ) ? strip_tags($_POST['group_id']) : 1;
+        $pitch_id = ( isset($_POST['pitch_id']) ) ? strip_tags($_POST['pitch_id']) : 1;
         $reservation = $this->api_model->getReservationByTime($reservation_year,$reservation_month,$reservation_day,$reservation_time,$group_id,$pitch_id);
         echo json_encode($reservation);
     }
 
     public function getReservationByDay(){
-        $reservation_year = ( isset($_POST['reservation_year']) ) ? $_POST['reservation_year'] : date("Y", time());
-        $reservation_month = ( isset($_POST['reservation_month']) ) ? $_POST['reservation_month'] : date("m", time());
-        $reservation_day = ( isset($_POST['reservation_day']) ) ? $_POST['reservation_day'] : date("d", time());
-        $group_id = ( isset($_POST['group_id']) ) ? $_POST['group_id'] : 1;
-        $pitch_id = ( isset($_POST['pitch_id']) ) ? $_POST['pitch_id'] : 1;
+        $reservation_year = ( isset($_POST['reservation_year']) ) ? strip_tags($_POST['reservation_year']) : date("Y", time());
+        $reservation_month = ( isset($_POST['reservation_month']) ) ? strip_tags($_POST['reservation_month']) : date("m", time());
+        $reservation_day = ( isset($_POST['reservation_day']) ) ? strip_tags($_POST['reservation_day']) : date("d", time());
+        $group_id = ( isset($_POST['group_id']) ) ? strip_tags($_POST['group_id']) : 1;
+        $pitch_id = ( isset($_POST['pitch_id']) ) ? strip_tags($_POST['pitch_id']) : 1;
         $reservation = $this->api_model->getReservationByDay($reservation_year,$reservation_month,$reservation_day,$group_id,$pitch_id);
         echo json_encode($reservation);
     }
 
     public function getPitchByGroup(){
-        $group = ( isset($_POST['group']) ) ? $_POST['group'] : '1';
+        $group = ( isset($_POST['group']) ) ? strip_tags($_POST['group']) : '1';
         $pitchs = $this->api_model->getPitchByGroup($group);
         echo json_encode($pitchs);
     }
 
     public function getGroup(){
-        $group_name = ( isset($_POST['group_name']) ) ? $_POST['group_name'] : 'escazu';
+        $group_name = ( isset($_POST['group_name']) ) ? strip_tags($_POST['group_name']) : 'escazu';
         $id_group = $this->api_model->getGroup($group_name);
         echo json_encode($id_group);
     }
 
     public function getTemporaryReservationState(){
-        $team_id = ( isset($_POST['team_id']) ) ? $_POST['team_id'] : '1';
-        $reservation_time = ( isset($_POST['reservation_time']) ) ? $_POST['reservation_time'] : '08-09';
-        $reservation_year = ( isset($_POST['reservation_year']) ) ? $_POST['reservation_year'] : date("Y", time());
-        $reservation_month = ( isset($_POST['reservation_month']) ) ? $_POST['reservation_month'] : date("m", time());
-        $reservation_day = ( isset($_POST['reservation_day']) ) ? $_POST['reservation_day'] : date("d", time());
-        $group_id = ( isset($_POST['group_id']) ) ? $_POST['group_id'] : 1;
-        $pitch_id = ( isset($_POST['pitch_id']) ) ? $_POST['pitch_id'] : 1;
+        $team_id = ( isset($_POST['team_id']) ) ? strip_tags($_POST['team_id']) : '1';
+        $reservation_time = ( isset($_POST['reservation_time']) ) ? strip_tags($_POST['reservation_time']) : '08-09';
+        $reservation_year = ( isset($_POST['reservation_year']) ) ? strip_tags($_POST['reservation_year']) : date("Y", time());
+        $reservation_month = ( isset($_POST['reservation_month']) ) ? strip_tags($_POST['reservation_month']) : date("m", time());
+        $reservation_day = ( isset($_POST['reservation_day']) ) ? strip_tags($_POST['reservation_day']) : date("d", time());
+        $group_id = ( isset($_POST['group_id']) ) ? strip_tags($_POST['group_id']) : 1;
+        $pitch_id = ( isset($_POST['pitch_id']) ) ? strip_tags($_POST['pitch_id']) : 1;
         $state = $this->api_model->getTemporaryReservationState($team_id,$reservation_time,$reservation_year,$reservation_month,$reservation_day,$group_id,$pitch_id);
         echo json_encode($state);
     }
 
     public function setTemporaryReservationState(){
-        $team_id = ( isset($_POST['team_id']) ) ? $_POST['team_id'] : '1';
-        $reservation_time = ( isset($_POST['reservation_time']) ) ? $_POST['reservation_time'] : '08-09';
-        $reservation_year = ( isset($_POST['reservation_year']) ) ? $_POST['reservation_year'] : date("Y", time());
-        $reservation_month = ( isset($_POST['reservation_month']) ) ? $_POST['reservation_month'] : date("m", time());
-        $reservation_day = ( isset($_POST['reservation_day']) ) ? $_POST['reservation_day'] : date("d", time());
-        $group_id = ( isset($_POST['group_id']) ) ? $_POST['group_id'] : 1;
-        $pitch_id = ( isset($_POST['pitch_id']) ) ? $_POST['pitch_id'] : 1;
-        $state = ( isset($_POST['state']) ) ? $_POST['state'] : 3;
+        $team_id = ( isset($_POST['team_id']) ) ? strip_tags($_POST['team_id']) : '1';
+        $reservation_time = ( isset($_POST['reservation_time']) ) ? strip_tags($_POST['reservation_time']) : '08-09';
+        $reservation_year = ( isset($_POST['reservation_year']) ) ? strip_tags($_POST['reservation_year']) : date("Y", time());
+        $reservation_month = ( isset($_POST['reservation_month']) ) ? strip_tags($_POST['reservation_month']) : date("m", time());
+        $reservation_day = ( isset($_POST['reservation_day']) ) ? strip_tags($_POST['reservation_day']) : date("d", time());
+        $group_id = ( isset($_POST['group_id']) ) ? strip_tags($_POST['group_id']) : 1;
+        $pitch_id = ( isset($_POST['pitch_id']) ) ? strip_tags($_POST['pitch_id']) : 1;
+        $state = ( isset($_POST['state']) ) ? strip_tags($_POST['state']) : 3;
         $this->api_model->setTemporaryReservationState($team_id,$reservation_time,$reservation_year,$reservation_month,$reservation_day,$group_id,$pitch_id,$state);
     }
 }
