4adf87b38a4bf5d1169a0c230693da7b259eacc1
Christian Campos <christian.campos@prodigious.cr>
2015-01-09 19:39:02 -0600

    stmp mail configured

--- application/controllers/email_controller.php
+++ application/controllers/email_controller.php
@@ -6,27 +6,31 @@ class Email_controller extends CI_Controller {
     {
         parent::__construct();
         $this->load->library('email');
-        /*$config['protocol'] = 'smtp';
-        $config['charset'] = 'utf-8';
-        $config['wordwrap'] = FALSE;
-        $config['mailtype'] = 'text';
-        $config['smtp_host'] = 'smtpout.secureserver.net';
+        $config['protocol'] = 'smtp';
+        
+        //$config['wordwrap'] = FALSE;
+        $config['smtp_host'] = 'ssl://smtpout.secureserver.net';
         $config['smtp_user'] = 'reserva@f5.cr';
-        $config['smtp_pass'] = 'G0D@ddy1254';
-        $config['smtp_port'] = '25';
-        $config['smtp_timeout'] = '5';*/
+        $config['smtp_pass'] = 'Reserva2014';
+        $config['smtp_port'] = 465;
+        $config['smtp_timeout'] = '5';
+        $config['mailtype'] = "text/plain";
+        //$config['validate'] = "TRUE";
+        $config['charset'] = 'iso-8859-1';
+        
         $this->email->initialize($config);
     }
 
     public function sendEmail(){
     	$email = ( isset($_POST['email']) ) ? strip_tags($_POST['email']) : '';
     	$data_reservation = ( isset($_POST['data_reservation']) ) ? strip_tags($_POST['data_reservation']) : '';
-        $this->email->from('reserva@f5.cr', 'F5 Reservaciones');
+        $this->email->from('reserva@f5.cr');
 		$this->email->to($email); 
 		/*$this->email->cc($email); 
 		$this->email->bcc($email);*/
-		$this->email->subject('Su Reservación se ha efectuado correctamente');
+		$this->email->subject('Su Reservacion se ha efectuado correctamente');
 		$this->email->message($data_reservation);	
 		$this->email->send();
+        echo $this->email->print_debugger();
     }
 }
