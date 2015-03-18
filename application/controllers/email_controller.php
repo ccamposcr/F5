e58b3abfdd40d4fcf3c78fb587113e466746f53f
Christian Campos <christian.campos@prodigious.cr>
2015-03-18 00:22:07 -0600

    .

--- application/controllers/email_controller.php
+++ application/controllers/email_controller.php
@@ -6,12 +6,16 @@ class Email_controller extends CI_Controller {
     {
         parent::__construct();
         $this->load->library('email');
-        /*$config['protocol'] = 'smtp';
-        $config['smtp_host'] = 'ssl://smtp.gmail.com';
-        $config['smtp_user'] = 'proyectohood@gmail.com';
-        $config['smtp_pass'] = 'proyectohood1234';
-        $config['smtp_port'] = 465 ;
-        $config['mailtype'] = "html";
+        //$config['protocol'] = 'smtp';
+        
+        //$config['wordwrap'] = FALSE;
+        /*$config['smtp_host'] = 'ssl://smtpout.secureserver.net';
+        $config['smtp_user'] = 'reserva@f5.cr';
+        $config['smtp_pass'] = 'Reserva2014';
+        $config['smtp_port'] = 465;
+        $config['smtp_timeout'] = '5';
+        $config['mailtype'] = "text/plain";
+        //$config['validate'] = "TRUE";
         $config['charset'] = 'iso-8859-1';
         
         $this->email->initialize($config);*/
@@ -22,7 +26,8 @@ class Email_controller extends CI_Controller {
     	$data_reservation = ( isset($_POST['data_reservation']) ) ? strip_tags($_POST['data_reservation']) : '';
         $this->email->from('reserva@f5.cr');
 		$this->email->to($email); 
-		$this->email->cc('reserva@f5.cr'); 
+		/*$this->email->cc($email); 
+		$this->email->bcc($email);*/
 		$this->email->subject('Su Reservacion se ha efectuado correctamente');
 		$this->email->message($data_reservation);	
 		$this->email->send();
