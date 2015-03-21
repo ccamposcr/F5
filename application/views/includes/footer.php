e8e7bd56a5f5b096d39e2aa532074585c96a44c6
CaroBorges <carobe18685@gmail.com>
2015-03-21 14:16:42 -0600

    cambios en el footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -5,7 +5,7 @@
 		            <a href="" class="fa fa-twitter"></a>
 		            <a href="" class="fa fa-youtube"></a>       
 		        </div>
-		        <p id="copy-right">© 2014Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur,esse rem ducimus itaque quis  dolor sit amet.</p>
+		        <p id="copy-right">© 2015  Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 		        <span id="siteseal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=cMzcYREMtupIS3ZxQa3zaq8M3sZ6FgK8xmOFZDbVVjvuzPXaBzvWXXHmV5Kx"></script></span>
 		    </div>
 		</div>
--- application/views/includes/internal_footer.php
+++ application/views/includes/internal_footer.php
@@ -26,7 +26,7 @@
 		            
 			    </div>
 
-		        <p id="copy-right">© 2014Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur, esse rem ducimus itaque quis.</p>
+		        <p id="copy-right">© 2014Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 		        <span id="siteseal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=cMzcYREMtupIS3ZxQa3zaq8M3sZ6FgK8xmOFZDbVVjvuzPXaBzvWXXHmV5Kx"></script></span>
 		    </div>
 		</div>
