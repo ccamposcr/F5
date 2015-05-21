f96c11f87b572e7a617070a868a0a7f0d00240c7
Christian Campos <christian.campos@prodigious.cr>
2015-05-20 23:55:48 -0600

    email fixed

--- application/controllers/email_controller.php
+++ application/controllers/email_controller.php
@@ -6,25 +6,24 @@ class Email_controller extends CI_Controller {
     {
         parent::__construct();
         $this->load->library('email');
-        //$config['protocol'] = 'smtp';
+        $config['protocol'] = 'smtp';
         
-        //$config['wordwrap'] = FALSE;
-        /*$config['smtp_host'] = 'ssl://smtpout.secureserver.net';
+        $config['wordwrap'] = TRUE;
+        $config['smtp_host'] = 'ssl://smtpout.secureserver.net';
         $config['smtp_user'] = 'reserva@f5.cr';
         $config['smtp_pass'] = 'Reserva2014';
-        $config['smtp_port'] = 465;
-        $config['smtp_timeout'] = '5';
-        $config['mailtype'] = "text/plain";
-        //$config['validate'] = "TRUE";
-        $config['charset'] = 'iso-8859-1';
+        $config['smtp_port'] = '465';
+        $config['mailtype'] = 'html';
+        $config['charset'] = 'utf-8';
+        $config['newline'] = "\r\n";
         
-        $this->email->initialize($config);*/
-        $config['protocol'] = 'sendmail';
+        $this->email->initialize($config);
+        /*$config['protocol'] = 'sendmail';
         $config['mailpath'] = '/usr/sbin/sendmail';
         $config['charset'] = 'iso-8859-1';
         $config['wordwrap'] = TRUE;
 
-        $this->email->initialize($config);
+        $this->email->initialize($config);*/
     }
 
     public function sendEmail(){
@@ -32,8 +31,7 @@ class Email_controller extends CI_Controller {
     	$data_reservation = ( isset($_POST['data_reservation']) ) ? strip_tags($_POST['data_reservation']) : '';
         $this->email->from('reserva@f5.cr');
 		$this->email->to($email); 
-		/*$this->email->cc($email); 
-		$this->email->bcc($email);*/
+		$this->email->cc('reserva@f5.cr'); 
 		$this->email->subject('Su reservación se ha efectuado correctamente');
 		$this->email->message($data_reservation);	
 		$this->email->send();
