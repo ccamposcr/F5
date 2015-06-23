91e1c77da78fea9760e988243c13510845a7c57e
Christian Campos <christian.campos@prodigious.cr>
2015-06-23 00:31:42 -0600

    SMS implemented

--- application/controllers/email_controller.php
+++ application/controllers/email_controller.php
@@ -29,7 +29,7 @@ class Email_controller extends CI_Controller {
         $data_reservation = ( isset($_POST['data_reservation']) ) ? $_POST['data_reservation'] : '';
         $this->email->from('reserva@f5.cr', 'Reservaciones F5');
         $this->email->to($email); 
-        $this->email->cc('avega@universal.co.cr'); 
+        $this->email->cc('reserva@f5.cr'); 
         $this->email->subject('F5 | Su reservación se ha efectuado correctamente');
         $this->email->message($data_reservation);
         $this->email->send();
@@ -55,15 +55,15 @@ class Email_controller extends CI_Controller {
         $data_reservation = ( isset($_POST['data_reservation']) ) ? $_POST['data_reservation'] : '';
         $this->email->from('reserva@f5.cr', 'Reservaciones F5');
         $this->email->to('dualimentos@tec-sms.com'); 
-        //$this->email->subject($phone);
-        $this->email->subject('88308780');
+        $this->email->subject($phone);
+        //$this->email->subject('88308780');
         $this->email->message($data_reservation);
         $this->email->send();
         echo $this->email->print_debugger();
 
-        $this->email->subject('83180160');
+        /*$this->email->subject('83180160');
         $this->email->message($data_reservation);
         $this->email->send();
-        echo $this->email->print_debugger();
+        echo $this->email->print_debugger();*/
     }
 }
