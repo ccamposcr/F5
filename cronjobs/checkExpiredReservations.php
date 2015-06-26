ee2b0d5f5c1205694233265b69412facbe8dd334
Christian Campos <christian.campos@prodigious.cr>
2015-06-26 12:52:34 -0600

    joncrob fixed

--- cronjobs/checkExpiredReservations.php
+++ cronjobs/checkExpiredReservations.php
@@ -1,5 +1,5 @@
 <?php
-	$curl = curl_init('f5.cr/stage/checkExpiredReservations');
+	$curl = curl_init('f5.cr/checkExpiredReservations');
 	$resp = curl_exec($curl);
 	curl_close($curl);
 	echo $resp;
